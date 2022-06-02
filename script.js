e7402c6648f1fb6e21bc463f9d233596e055b5d3
dlhardi <derricklhardison@gmail.com>
2022-06-02 16:30:33 -0400

    added kiloPounds function

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
+// DOM Variables
 mainNumber = +document.getElementById("main-number").textContent;
 numFeet = document.getElementById("num-feet");
 meterSentence = document.getElementById("meter-feet-sentence");
+literGalSentence = document.getElementById("liter-gal-sentence");
+kiloPoundSentence = document.getElementById("kilo-pound-sentence");
 
-console.log(mainNumber);
-console.log(meterSentence.textContent);
-
-// mainNumber.textContent = "";
-
-// 1 meter = 3.280 foot
+// Variables
 let convertedNumFeet = 0;
 let convertedNumMeter = 0;
+
+let convertedNumGal = 0;
+let convertedNumLit = 0;
+
+let convertedNumPounds = 0;
+let convertedNumKilo = 0;
+
+// Functions
 function meterFeet() {
   convertedNumFeet = mainNumber * 3.281;
   convertedNumMeter = mainNumber * 0.304;
-  // numFeet.textContent = convertedNumFeet;
-
-  meterSentence.textContent = `${mainNumber} meters = ${convertedNumFeet} | ${mainNumber} feet = ${convertedNumMeter} meters`;
+  meterSentence.textContent = `${mainNumber} meters = ${convertedNumFeet} feet | ${mainNumber} feet = ${convertedNumMeter} meters`;
 }
+
+function literGal() {
+  convertedNumGal = mainNumber * 0.264;
+  convertedNumLit = mainNumber * 3.785;
+  literGalSentence.textContent = `${mainNumber} liters = ${convertedNumGal} gallons | ${mainNumber} gallons = ${convertedNumLit} liters`;
+}
+function kiloPounds() {
+  convertedNumPounds = mainNumber * 2.204;
+  convertedNumKilo = mainNumber * 0.453;
+  kiloPoundSentence.textContent = `${mainNumber} liters = ${convertedNumPounds} pounds | ${mainNumber} pounds = ${convertedNumKilo} kilos`;
+}
+
+// Function calls
 meterFeet();
-console.log(convertedNumFeet);
+literGal();
+kiloPounds();
