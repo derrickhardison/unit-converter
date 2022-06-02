bc51cbce04a46be83937aadb912b2315dd6b6f89
dlhardi <derricklhardison@gmail.com>
2022-06-02 16:07:12 -0400

    testing different ways of manipulating the DOM

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("attached");
+mainNumber = +document.getElementById("main-number").textContent;
+numFeet = document.getElementById("num-feet");
+meterSentence = document.getElementById("meter-feet-sentence");
+
+console.log(mainNumber);
+console.log(meterSentence.textContent);
+
+// mainNumber.textContent = "";
+
+// 1 meter = 3.280 foot
+let convertedNumFeet = 0;
+let convertedNumMeter = 0;
+function meterFeet() {
+  convertedNumFeet = mainNumber * 3.281;
+  convertedNumMeter = mainNumber * 0.304;
+  // numFeet.textContent = convertedNumFeet;
+
+  meterSentence.textContent = `${mainNumber} meters = ${convertedNumFeet} | ${mainNumber} feet = ${convertedNumMeter} meters`;
+}
+meterFeet();
+console.log(convertedNumFeet);
